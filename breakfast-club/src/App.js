fe3d20c7b6f211e4de12e7a2b08bb0f9360442c0
Bang Pham <bangpham@Bangs-MBP.attlocal.net>
2024-10-15 16:28:50 -0400

    create react app for front end

---
+++ breakfast-club/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Breakfast Club</h1>
+      <p>Where the breakfast is always served hot!</p>
+    </div>
+  );
+}
+
+export default App;
