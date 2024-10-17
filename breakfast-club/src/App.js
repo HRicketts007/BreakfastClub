349af6281635b85b8e085d450c480bfe236d818d
Bang Pham <bangpham@Bangs-MBP.attlocal.net>
2024-10-16 22:16:37 -0400

    cards

--- breakfast-club/src/App.js
+++ breakfast-club/src/App.js
@@ -1,15 +1,19 @@
 
 import "./App.css";
+import Nav from "./components/Nav";
 import MealPlanner from "./containers/MealPlanner";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
-        <MealPlanner />
-      </header>
-    </div>
+   
+       <>
+        <Nav />
+       <div className="container d-flex flex-column justify-content-center align-items-center " style={{height:"100vh"}}>
+          <MealPlanner />
+        </div>
+
+        </>
+   
   );
 }
 
--- breakfast-club/src/containers/MealPlanner.jsx
+++ breakfast-club/src/containers/MealPlanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {  useState } from "react";
 import axios from "axios";
 
 const MealPlanner = () => {
@@ -6,21 +6,24 @@ const MealPlanner = () => {
   const [servings, setServings] = useState("");
   const [mealPlan, setMealPlan] = useState(null);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [expandedSummaries, setExpandedSummaries] = useState({});
 
-
-  // Function to generate a meal plan for the day
+  
+  //generate meal plan for day
   const generateDay = async () => {
-    //check input
+    //validate inputs
     if (caloricIntake <= 0 || servings <= 0) {
       setError(
         "Daily Caloric Intake and Servings Per Day must be greater than zero."
       );
       return;
     }
-    //reset error
+    //reset error & loading
     setError("");
+    setLoading(true);
 
-    // Call the API to generate the meal plan
+    //api call to get meal plans by day
     try {
       const response = await axios.get(
         "http://194.195.210.118:5000/Generate_Day",
@@ -31,79 +34,136 @@ const MealPlanner = () => {
           },
         }
       );
-      // Set the meal plan in the state
+      //set meal plan in state
       setMealPlan(response.data.meal_plan);
-     
+      console.log(response.data.meal_plan);
     } catch (error) {
-      // Log the error and set the error message in the state
+      //handle error
       console.error("Error generating meal plan:", error);
       setError("Failed to generate meal plan. Please try again later.");
-      
+    } finally {
+      setLoading(false);
     }
   };
 
-  // Function to clean HTML content from API response
-  const cleanHTML = (htmlContent) => {
-    const tempDiv = document.createElement("div");
-    tempDiv.innerHTML = htmlContent;
-    return tempDiv.textContent || tempDiv.innerText || "";
+  //format html content to plain text 
+  const formatData = (htmlContent) => {
+    const temp = document.createElement("div");
+    temp.innerHTML = htmlContent;
+    return temp.textContent || temp.innerText || "";
   };
 
- 
+  //show recipe description 
+  const toggleSummary = (index) => {
+    setExpandedSummaries((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
+
   return (
-    <div className="container bg-white rounded-4 p-3 shadow-lg">
-          <h1 className="fw-bold text-warning">Breakfast Club</h1>
-      <h2 className="fw-bold">Meal Planner</h2>
-      <p>Input your calorie goals and target servings per day</p>
-     <div className="d-flex flex-column ">
-     <input
-        type="number"
-        placeholder="Caloric Intake"
-        value={caloricIntake}
-        onChange={(e) => setCaloricIntake(e.target.value)}
-      />
-      <br />
-      <input
-        type="number"
-        placeholder="Servings Per Day"
-        value={servings}
-        onChange={(e) => setServings(e.target.value)}
-      />
-      <br />
-      <button className="btn btn-warning" onClick={generateDay}>Generate Meal Plan</button>
-     </div>
-      {/* display error */}
-      {error && <div style={{ color: "red", marginTop: "10px" }}>{error}</div>}
+    <>
+      <div className="container bg-white rounded-4 p-3 shadow-lg">
+       
+        <h2 className="fw-bold">Meal Planner</h2>
+        <p>Input your calorie goals and target servings per day</p>
+        <div className="d-flex flex-column ">
+          <input
+          className="form-control"
+            type="number"
+            placeholder="Caloric Intake"
+            value={caloricIntake}
+            onChange={(e) => setCaloricIntake(e.target.value)}
+          />
+          <br />
+          <input
+          className="form-control"
+
+            type="number"
+            placeholder="Servings Per Day"
+            value={servings}
+            onChange={(e) => setServings(e.target.value)}
+          />
+          <br />
+          <button className="btn btn-warning d-flex flex-row justify-content-center align-items-center" style={{height: "50px"}} onClick={generateDay}>
+          {loading ?  (
+        <div className="text-center text-white my-3">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : "Generate Meal Plan"}
+          </button>
+        </div>
+        {/* display error */}
+        {error && (
+          <div style={{ color: "red", marginTop: "10px" }}>{error}</div>
+        )}
+      </div>
+    
+
       {/* display meal plan */}
       {mealPlan && (
-        <div>
-          <h3>Your Meal Plan for the Day</h3>
-          <ul>
-            {Object.keys(mealPlan).map((key, index) => {
-              const recipe = mealPlan[key]?.Information;
-              if (recipe) {
-                return (
-                  <li key={index}>
-                    <h4>{recipe.title}</h4>
-                    <p>{cleanHTML(recipe.summary)}</p>
-                    <p>
-                      Serves: {recipe.servings} | Prep Time:{" "}
-                      {recipe.readyInMinutes} minutes
-                    </p>
-                    <p>
-                      Calories: {recipe.nutrition?.calories} | Protein:{" "}
-                      {recipe.nutrition?.protein} | Fat: {recipe.nutrition?.fat}
-                    </p>
-                  </li>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </ul>
-        </div>
+        <>
+          <br />
+          <div className="container bg-white rounded-4 p-3 shadow-lg ">
+            <h3>Your Meal Plan for the Day</h3>
+
+            <div className="row">
+              {/* display each recipe */}
+              {Object.keys(mealPlan).map((key, index) => {
+                const recipe = mealPlan[key]?.Information;
+                const instructions = mealPlan[key]?.Instructions;
+                const nutrition = mealPlan[key]?.Nutrition;
+                if (recipe ) {
+                  const summary = formatData(recipe.summary);
+                  const isExpanded = expandedSummaries[index];
+                  const truncatedSummary = summary.slice(0, 100);
+                  console.log(recipe)
+                  // console.log(instructions)
+                  console.log(nutrition)
+
+                  return (
+                    <div className="col mb-4" key={index}>
+                      <div className="card h-100" style={{ width: "18rem" }}>
+                      <img src={recipe.image} className="card-img-top" alt="Recipe Img"/>
+                        <div className="card-body d-flex flex-column">
+                          <h4>{recipe.title}</h4>
+                          <p>
+                            {isExpanded ? summary : `${truncatedSummary}...`}
+                            {summary.length > 100 && (
+                              <button
+                                className="btn btn-link p-0"
+                                onClick={() => toggleSummary(index)}
+                              >
+                                {isExpanded ? "View Less" : "View More"}
+                              </button>
+                            )}
+                          </p>
+                          <p>
+                            Serves: {recipe.servings} | Prep Time: {" "}
+                            {recipe.readyInMinutes} minutes
+                          </p>
+                          <p>
+                            Calories: {nutrition.calories} | Protein: 
+                            {nutrition.protein} | Fat: {nutrition.fat} | Carbs: {nutrition.carbs}
+                          </p>
+                          <div className="mt-auto">
+                            <button className="btn btn-warning">View Recipe</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+          </div>
+        </>
       )}
-    </div>
+    </>
   );
 };
 
